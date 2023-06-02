ab6b7cd027d009a3358af36f1fdbbe98d82bdc6a
liuhao <liuhao@git.ixuenong.com>
2023-06-02 11:40:55 +0800

    socket server

--- server.php
+++ server.php
@@ -27,3 +27,74 @@ function basicSocket()
         }
     }
 }
+
+function oneClientSocket()
+{
+    $host = '0.0.0.0';
+    $port = 9999;
+    $listenSocket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+    socket_bind($listenSocket, $host, $port);
+
+    socket_listen($listenSocket);
+    while (true) {
+        $connectionSocket = socket_accept($listenSocket);
+        $msg = "hello world \r\n";
+        socket_write($connectionSocket, $msg, strlen($msg));
+        socket_close($connectionSocket);
+    }
+    //socket_close($listenSocket);
+}
+
+function manyClientSocket()
+{
+    $host = '0.0.0.0';
+    $port = 9999;
+
+    $listenSocket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+    socket_bind($listenSocket, $host, $port);
+    socket_listen($listenSocket);
+
+    while (true) {
+        $connectionSocket = socket_accept($listenSocket);
+        $pid = pcntl_fork();
+        if (0 == $pid) {
+            $msg = "hello world\r\n";
+            socket_write($connectionSocket, $msg, strlen($msg));
+            echo time() . ': a new client' . PHP_EOL;
+            sleep(5);
+            socket_close($connectionSocket);
+            exit;
+        }
+    }
+    //socket_close($listenSocket);
+}
+
+function repeatClientSocket()
+{
+    $host = '0.0.0.0';
+    $port = 9999;
+    $listenSocket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+    socket_bind($listenSocket, $host, $port);
+    socket_listen($listenSocket);
+    cli_set_process_title('php server master process');
+    for ($i = 1; $i <= 10; $i++) {
+        $pid = pcntl_fork();
+        if (0 == $pid) {
+            cli_set_process_title('php server worker process');
+            while (true) {
+                $connSocket = socket_accept($listenSocket);
+                $msg = "hello world\r\n";
+                socket_write($connSocket, $msg, strlen($msg));
+                socket_close($connSocket);
+            }
+        }
+    }
+
+    while (true) {
+        sleep(1);
+    }
+
+    //socket_close($listenSocket);
+}
+
+repeatClientSocket();
