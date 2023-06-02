7ef57b5ab7f9cc99b37371789ea0de32c10275dc
liuhao <liuhao@git.ixuenong.com>
2023-06-02 11:09:29 +0800

    daemon

--- server.php
+++ server.php
@@ -1,26 +1,29 @@
 <?php
 
-$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-if (!socket_bind($socket, '127.0.0.1', 5000)) {
-    throw new AssertionError('listen port error, port:' . 5000);
-}
+function basicSocket()
+{
+    $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+    if (!socket_bind($socket, '127.0.0.1', 5000)) {
+        throw new AssertionError('listen port error, port:' . 5000);
+    }
 
-if (socket_listen($socket, 4) == false) {
-    throw new AssertionError('listen port error, port:' . 5000);
-}
+    if (socket_listen($socket, 4) == false) {
+        throw new AssertionError('listen port error, port:' . 5000);
+    }
 
-while (true) {
-    $acceptResource = socket_accept($socket);
-    if ($acceptResource !== false) {
-        $string = socket_read($acceptResource, 1024);
+    while (true) {
+        $acceptResource = socket_accept($socket);
+        if ($acceptResource !== false) {
+            $string = socket_read($acceptResource, 1024);
 
-        if ($string != false) {
-            $returnClient = 'server receive is : ' . $string . PHP_EOL;
-            socket_write($acceptResource, $returnClient, strlen($returnClient));
-        } else {
-            echo 'socket_read is fail';
+            if ($string != false) {
+                $returnClient = 'server receive is : ' . $string . PHP_EOL;
+                socket_write($acceptResource, $returnClient, strlen($returnClient));
+            } else {
+                echo 'socket_read is fail';
+            }
+
+            socket_close($acceptResource);
         }
-
-        socket_close($acceptResource);
     }
 }
